9e460a4f1fba22870833703f6023849abacf5c14
David Barozzini <david.barozzini@grupposcudo.it>
2022-08-23 15:00:27 +0000

    version bump

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     "license": "Other proprietary",
     "author": "Gruppo Scudo Srl, David Barozzini",
     "website": "http://www.grupposcudo.it",
-    "version": "14.0.0.3.2",
+    "version": "14.0.0.3.3",
     "depends": ["base"],
     "external_dependencies": {"python": ["boto3"]},
     "data": [
